10b850417ec718077a74a43ca642ed0e607125c0
Caio Libanio <caiolibanio@gmail.com>
2014-02-23 22:58:08 +0000

    TestBST2.java edited online with Bitbucket

--- src/adt/bst/TestBST2.java
+++ src/adt/bst/TestBST2.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class TestBST2 {
 	//lascou
-	//conflitohahakkkresolvido
+	//conflitocc
 	protected BSTImpl<Integer, Integer> tree1;
 	protected BSTImpl<Integer, Integer> tree2;
 	protected BSTImpl<Integer, Integer> tree3;
