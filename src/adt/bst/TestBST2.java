46b365f149368e88b047bd071dce8c15bcba44db
caiolibanio <caiolibanio@gmail.com>
2014-02-23 20:40:49 -0300

    mudanças

--- src/adt/bst/TestBST2.java
+++ src/adt/bst/TestBST2.java
@@ -6,8 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestBST2 {
-	//conflito
-	//teste
+	//conflitouhhu
+	//testee
 	protected BSTImpl<Integer, Integer> tree1;
 	protected BSTImpl<Integer, Integer> tree2;
 	protected BSTImpl<Integer, Integer> tree3;
