55393c852ba5f9c46ab2213d99771321b4d44629
caiolibanio <caiolibanio@gmail.com>
2014-02-23 19:49:13 -0300

    teste

--- src/adt/bst/TestBST2.java
+++ src/adt/bst/TestBST2.java
@@ -7,9 +7,7 @@ import org.junit.Test;
 
 public class TestBST2 {
 	//lascou
-	//lascou1w
-	//conflito
-	//conflito
+	//conflitohaha
 	protected BSTImpl<Integer, Integer> tree1;
 	protected BSTImpl<Integer, Integer> tree2;
 	protected BSTImpl<Integer, Integer> tree3;
